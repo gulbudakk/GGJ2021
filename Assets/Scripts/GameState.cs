0bef53adba618060399727fac216997129b70e13
Kalender Gülbudak <gulbudakk@gmail.com>
2021-01-30 02:47:15 +0300

    Radar

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [Header("- - - References - - -")]
+
+    public GameObject Character;
+
+    [Header("- - - Variables - - -")]
+
+    public bool DebugActive;
+    void Start()
+    {
+        
+    }
+
+}
