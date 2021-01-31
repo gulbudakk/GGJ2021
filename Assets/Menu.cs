56348305d50547920c85d6f25386790df88d50ea
Kalender Gülbudak <gulbudakk@gmail.com>
2021-01-31 16:12:43 +0300

    manu

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void startClick() 
+    {
+        SceneManager.GetSceneByName("Main");
+    }
+
+    public void quitClick() 
+    {
+        Application.Quit();
+    }
+}
